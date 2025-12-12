5a446e07c83c55cdaa6c67243bac95b0ed709fcc
ausdotsn50 <ausdotsn50@gmail.com>
2025-12-12 10:21:07 +0800

    wip: casio and battery

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ const calcHeight = 600;
 const viewHeight = calcHeight * 0.30;
 const btnHeight = calcHeight * 0.70;
 
-root.style.setProperty('--calc-width', calcWidth + "px");
+const viewHeaderHeight = viewHeight * 0.20;
+const viewScreenHeight = viewHeight * 0.80;
+
+root.style.setProperty('--calc-width', calcWidth+ "px");
 root.style.setProperty('--calc-height', calcHeight+ "px");
 
 root.style.setProperty('--view-height', viewHeight+ "px");
 root.style.setProperty('--btn-height', btnHeight+ "px");
+
+root.style.setProperty('--view-hdr-height', viewHeaderHeight+ "px");
+root.style.setProperty('--view-scrn-height', viewScreenHeight+ "px");
 
 const calcBody = document.querySelector("#calculator");
